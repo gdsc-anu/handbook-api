6082cb032c317339607e2e239c64d0bf4c9793a4
👨‍💻 Mathias Elorm <94559474+mathiaselorm@users.noreply.github.com>
2024-03-26 23:23:03 +0000

    .

--- student_handbook/settings.py
+++ student_handbook/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tnp0h_c*=1l=00o+$xpj5ey@^i*pf-$7xx*jb7pn43n^m9(!9v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['anu-handbook-5da7fbd32316.herokuapp.com', '127.0.0.1']
 
